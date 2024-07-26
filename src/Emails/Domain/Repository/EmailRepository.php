22ce3d1daa88f22d49b39e82fd4f2d25269e5b92
maxgoover <maxgoover@gmail.com>
2024-07-26 19:32:36 +0300

    installed doctrine + added postgres + added module Emails

---
+++ src/Emails/Domain/Repository/EmailRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Emails\Domain\Repository;
+
+interface EmailRepositoryInterface
+{
+
+}
---
+++ src/Emails/Infrastructure/Repository/EmailRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Emails\Infrastructure\Repository;
+
+use App\Emails\Domain\Repository\EmailRepositoryInterface;
+
+class EmailRepository implements EmailRepositoryInterface
+{
+
+}
